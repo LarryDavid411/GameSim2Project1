21e163d9294c91ebeadf3565dfef63f3ce6ad09c
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-02-08 17:51:56 -0600

    Animation for Walking and Attacking Completed
    Next need to add objects to attack or create fog of death.

--- Project1/Assets/FSMController.cs
+++ Project1/Assets/FSMController.cs
@@ -6,16 +6,19 @@ public class FSMController : MonoBehaviour
 {
 
     public GameObject player;
-
+    
     public enum State
     {
         Idle,
         Walking,
+        Attacking,
+        AttackingWhileWalking,
         ActivelyMoving
     }
 
     public State state;
-    
+    public bool playerAttacking;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,20 +36,96 @@ public class FSMController : MonoBehaviour
         {
             case State.Idle:
             {
+                // play idle animation
+                player.GetComponent<Animator>().SetBool("isWalking", false);
+                // state to walking
                 if (moveX != 0 || moveY != 0)
                 {
                     state = State.Walking;
-                }    
+                }
+                if (Input.GetKey("space"))
+                {
+                    state = State.Attacking;
+                    player.GetComponent<Animator>().SetBool("isAttacking", true);
+                }
+                else
+                {
+                    player.GetComponent<Animator>().SetBool("isAttacking", false);
+                }
             }
                 break;
 
             case State.Walking:
             {
-                if (moveX == 0 || moveY == 0)
+                // flip direction of player
+                if (moveX < 0)
+                {
+                    player.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else if (moveX > 0)
+                {
+                    player.GetComponent<SpriteRenderer>().flipX = false;
+                }
+                // play walking animation
+                player.GetComponent<Animator>().SetBool("isWalking", true);
+                // state to idle
+                if (moveX == 0 && moveY == 0)
                 {
                     state = State.Idle;
                 }
+                
+                if (Input.GetKey("space"))
+                {
+                    state = State.AttackingWhileWalking;
+                }
+                
+                // set to move player
+                moveDir.x = moveX;
+                moveDir.y = moveY;
+            }
+                break;
 
+
+            case State.Attacking:
+            {
+                player.GetComponent<Animator>().SetBool("isAttacking", true);
+                if (Input.GetKeyUp("space"))
+                {
+                    state = State.Idle;
+                    player.GetComponent<Animator>().SetBool("isAttacking", false);
+                }
+
+                if (moveX != 0 || moveY != 0)
+                {
+                    state = State.AttackingWhileWalking;
+                }
+              
+            }
+                break;
+
+            case State.AttackingWhileWalking:
+            {
+                player.GetComponent<Animator>().SetBool("isWalking", false);
+                player.GetComponent<Animator>().SetBool("isAttacking", true);
+                if (Input.GetKeyUp("space"))
+                {
+                    state = State.Idle;
+                    player.GetComponent<Animator>().SetBool("isAttacking", false);
+                }
+                if (moveX != 0 && moveY != 0)
+                {
+                    state = State.Attacking;
+                }
+                // flip direction of player
+                if (moveX < 0)
+                {
+                    player.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else if (moveX > 0)
+                {
+                    player.GetComponent<SpriteRenderer>().flipX = false;
+                }
+                // set to move player
                 moveDir.x = moveX;
                 moveDir.y = moveY;
             }
@@ -54,7 +133,7 @@ public class FSMController : MonoBehaviour
         }
         
         
-        
+        // move player controller
         CharacterController c = player.GetComponent<CharacterController>();
         c.Move(moveDir * Time.deltaTime);
     }
