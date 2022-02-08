b31cec66e30d19b12e9509b5d6bf0eddfa0ed086
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-02-08 16:09:02 -0600

    Added Char Controller

--- Project1/Assets/FSMController.cs
+++ Project1/Assets/FSMController.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class FSMController : MonoBehaviour
 {
+
+    public GameObject player;
+
+    public enum State
+    {
+        Idle,
+        Walking,
+        ActivelyMoving
+    }
+
+    public State state;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.Idle;
     }
 
+
+    public void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3();
+
+        switch (state)
+        {
+            case State.Idle:
+            {
+                if (moveX != 0 || moveY != 0)
+                {
+                    state = State.Walking;
+                }    
+            }
+                break;
+
+            case State.Walking:
+            {
+                if (moveX == 0 || moveY == 0)
+                {
+                    state = State.Idle;
+                }
+
+                moveDir.x = moveX;
+                moveDir.y = moveY;
+            }
+                break;
+        }
+        
+        
+        
+        CharacterController c = player.GetComponent<CharacterController>();
+        c.Move(moveDir * Time.deltaTime);
+    }
     // Update is called once per frame
     void Update()
     {
+        
+        
         
     }
 }
