7c49df24d90167ad8dab51cede88a47e115cb3b8
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-02-13 11:02:26 -0600

    Proper Timing for Main Menu - Need to set mouse input

--- Project1/Assets/DialogueManager.cs
+++ Project1/Assets/DialogueManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-
+    //public GameObject gameManager;
     private float textTimerForLetters;
 
+    public int currentLevel; 
     private int displayTextSequence;
 
     public Text displayText;
@@ -27,58 +28,103 @@ public class DialogueManager : MonoBehaviour
 
     public string startGameTextString;
     
+    public enum MenuState
+    {
+        DelayStart,
+        StartTitle,
+        DelayPhase2,
+        DisplayAll
+    }
+
+    public MenuState menuState;
     // Start is called before the first frame update
     void Start()
     {
+        menuState = MenuState.DelayStart;
         titleOver = false;
         nameText = "uninhabited";
         nameTextAdded = "";
         displayTextSequence = -1;
         startGameTextString = "Start Game";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        textTimerForLetters += Time.deltaTime;
-        if (delayStartTimer < delayStart)
+        if (currentLevel == 0)
         {
-            delayStartTimer += Time.deltaTime;
-            if (delayStartTimer > delayStart)
+            switch (menuState)
             {
-                startTitle = true;
-            }
-        }
-        if ( startTitle && (displayTextSequence < nameText.Length - 1))
-        {
-            if (textTimerForLetters > textTimerDisplayRate)
-            {
-                displayTextSequence++;
-                textTimerForLetters = 0;
-                c = nameText[displayTextSequence];
-                nameTextAdded += c.ToString();
-            }
-        }
-        else
-        {
-            titleOverTimer += Time.deltaTime;
-            if (titleOverTimer > titleOverTimeSet)
-            {
-                titleOver = true;
+                case MenuState.DelayStart:
+                {
+                    delayStartTimer += Time.deltaTime;
+                    if (delayStartTimer > delayStart)
+                    {
+                        delayStartTimer = 0;
+                        menuState = MenuState.StartTitle;
+                    }
+                } break;
+
+                case MenuState.StartTitle:
+                {
+                    textTimerForLetters += Time.deltaTime;
+                    if (displayTextSequence < nameText.Length - 1)
+                    {
+                        if (textTimerForLetters > textTimerDisplayRate)
+                        {
+                            displayTextSequence++;
+                            textTimerForLetters = 0;
+                            c = nameText[displayTextSequence];
+                            nameTextAdded += c.ToString();
+                        }
+                    }
+                    else
+                    {
+                        menuState = MenuState.DelayPhase2;
+                    }
+                } break;
+
+                case MenuState.DelayPhase2:
+                {   
+                    delayStartTimer += Time.deltaTime;
+                    if (delayStartTimer > delayStart)
+                    {
+                        delayStartTimer = 0;
+                        menuState = MenuState.DisplayAll;
+                    }
+                } break;
+                
+                case MenuState.DisplayAll:
+                {
+                    displayTextStartButton.text = startGameTextString;
+                    
+                    
+                    
+                } break;
+                
             }
             
+            displayText.text = nameTextAdded;
+            
+            // if (delayStartTimer < delayStart)
+            // {
+            //     
+            // }
+            // if ( startTitle && (displayTextSequence < nameText.Length - 1))
+            // {
+            //     
+            // }
+            // else
+            // {
+            //     titleOverTimer += Time.deltaTime;
+            //     if (titleOverTimer > titleOverTimeSet)
+            //     {
+            //         titleOver = true;
+            //     }
+            //     Debug.Log("titleOver");
+            // }
+            
         }
-        
-        
-        
-        
-        displayText.text = nameTextAdded;
-
-        if (titleOver)
-        {
-            displayTextStartButton.text = startGameTextString;
-        }
-        
-        
     }
 }
