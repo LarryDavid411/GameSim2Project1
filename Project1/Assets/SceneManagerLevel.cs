c9cf171503840dbb4c6ffd2b5a751def4e7e9935
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-02-15 17:48:23 -0600

    Not sure why having problems with Scene2Border and Sprite of Barrier

--- Project1/Assets/SceneManagerLevel.cs
+++ Project1/Assets/SceneManagerLevel.cs
@@ -8,7 +8,7 @@ public class SceneManagerLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentLevel = 0;
+        //currentLevel = 0;
     }
 
     // Update is called once per frame
