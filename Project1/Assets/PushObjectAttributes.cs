9ba07d80ca8a8158b6fa1cbc277e0a61db4f2f35
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-02-19 14:07:52 -0600

    Revert "ObjectPushingWorks"
    This reverts commit 84b7be2e63203715f3e472376ca11f51ad09a464.

--- Project1/Assets/PushObjectAttributes.cs
+++ Project1/Assets/PushObjectAttributes.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PushObjectAttributes : MonoBehaviour
 {
     public bool introPushObject;
-    public float brickSpeed;
-    public bool pushingBrick;
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +15,6 @@ public class PushObjectAttributes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pushingBrick)
-        {
-            gameObject.transform.position += (Vector3.right * Time.deltaTime * brickSpeed);
-        }
+        
     }
 }
